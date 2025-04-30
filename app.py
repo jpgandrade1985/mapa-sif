c7d27ec67dd3bf08cec8d861111eb399d0a50495
jpgandrade1985 <jpgandrade@gmail.com>
2025-04-30 10:50:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ fig.add_trace(go.Scattermapbox(
     lon=df_estab_plot["longitude"],
     mode="markers",
     marker=dict(size=20, color='LightSkyBlue'), 
-    line=dict(color='MediumPurple', width=3),
+    line=dict(color='MediumPurple', width=7),
     hovertext=df_estab_plot["hover"],
     hoverinfo="text",
     showlegend=False
